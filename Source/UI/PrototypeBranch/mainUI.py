fd9a8928aeee65a2a5ce6e1145ef50c12cb25c06
Luxtylo <gb@gbryant.co.uk>
2013-05-16 08:13:47 +0100

    Started writing the main UI code in Source/UI/PrototypeBranch

--- Source/UI/PrototypeBranch/mainUI.py
+++ Source/UI/PrototypeBranch/mainUI.py
@@ -2,32 +2,75 @@
 
 ## Imports
 from Tkinter import *
+from tkFileDialog import askopenfilename
+import makeThe
 
 ## Initialise NoteApp class
 class NoteApp:
 
 	# Constructor
 	def __init__(self, master):
-		# Create frame
-		frame = Frame(master)
-		# Pack frame to contents
-		frame.pack()
 
-		# Make self.button, which is a button widget - runs frame.quit
-		self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
-		self.button.pack(side=LEFT)
+		# Create textbox frame and contents
+		self.makeTextboxFrame(master)
 
-		# Make self.hi_there, a button widget which runs self.say_hi
-		self.hi_there = Button(frame, text="Hello", command=self.say_hi)
-		self.hi_there.pack(side=LEFT)
+		# Create button frame and contents by calling makeButtonFrame
+		self.makeButtonFrame(master)
 
-	def say_hi(self):
-		print "hi there, everyone!"
+		# Initialise things as None
+		self.openLocation = None
+		self.inputFile = None
+		self.outputFile = None
 
-## Starting
+	# Create textbox frame and contents
+	def makeTextboxFrame(self, master):
+		textFrame = makeThe.tkFrame(master, 1)
 
-#Root widget
+		# Make textbox
+		self.textBox = Text(textFrame)
+		self.textBox.pack(fill = BOTH, expand = "yes")
+
+	# Create button frame and contents
+	def makeButtonFrame(self, master):
+		buttonFrame = makeThe.tkFrame(master, 2)
+
+		# Make self.openLoc, a button widget which runs self.askLocation
+		self.openButton = Button(buttonFrame, text = "Open", command = self.askLocation)
+		self.openButton.pack(side=LEFT)
+
+		# Make a button widget which runs self.printContents
+		self.saveButton = Button(buttonFrame, text = "Save", command = self.saveFile)
+		self.saveButton.pack(side = LEFT)
+
+		# Make self.quitButton, which is a button widget - runs frame.quit
+		self.quitButton = Button(buttonFrame, text = "Quit", fg = "red", command = buttonFrame.quit)
+		self.quitButton.pack(side=LEFT)
+
+	# Ask for location and open file
+	def askLocation(self):
+		# Run open dialog box to get filename
+		self.openLocation = askopenfilename(filetypes = [("Note files","*.note"),("Text files","*.txt")])
+
+		# If the filename is a blank string
+		if self.openLocation == "":
+			# Return exception here - new window saying "No file selected"
+			print ("Open Location Empty")
+		else:
+			# Open inputFile
+			self.inputFile = open (self.openLocation, "r")
+
+	def saveFile(self):
+		pass
+
+## STARTING
+
+# Root widget
 root = Tk()
+
+# Root widget properties
+root.title("Note") # Title in window title bar
+root.minsize(640,480) # Minimum size of window
+root.geometry("800x600") # Initial size of window
 
 # New instance of NoteApp
 app = NoteApp(root)
---
+++ Source/UI/PrototypeBranch/makeThe.py
@@ -0,0 +1,23 @@
+"""Functions to construct stuff to make it less repetitive"""
+
+# Import
+from Tkinter import *
+
+def tkFrame(master, packSide):
+	frame = Frame(master)
+
+	# packSide:
+	#	0 = not included
+	#	1 = LEFT
+	#	2 = RIGHT
+
+	if packSide == 0:
+		frame.pack(expand = BOTH)
+
+	elif packSide == 1:
+		frame.pack(side = LEFT, expand = BOTH)
+
+	elif packSide == 2:
+		frame.pack(side = RIGHT, expand = BOTH)
+
+	return frame
