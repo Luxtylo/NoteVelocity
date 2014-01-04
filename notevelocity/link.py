2da75c1e3a74e543e8611f261e714580d6c6e99b
George Bryant <gb@gbryant.co.uk>
2014-01-04 14:09:12 +0000

    Initial link window work
    getNoteSummaries() returns the titles and subtitles of the notes in the
    notes directory as a list
    
    This will be used to display the notes so the correct one can be
    selected for linking

--- notevelocity/link.py
+++ notevelocity/link.py
@@ -26,17 +26,74 @@ class LinkBox(Toplevel):
     def __init__(self, master):
         self.master = master
 
-        self.test = Toplevel()
+        self.initUI()
 
-        self.button = Button(self.test, text="Press me", command=self.getNoteList)
-        self.button.pack()
+        noteSummaries = self.getNoteSummaries()
 
-        self.test.lift()
+    def initUI(self):
+        self.window = Toplevel()
+
+        self.button = Button(self.window,
+            text="Close",
+            command=self.close)
+        self.button.pack(side=BOTTOM, anchor=SE)
+        
+        self.canvasFrame = Frame(self.window,
+            width=400,
+            height=300)
+        self.canvasFrame.pack(side=TOP)
+
+        self.canvas = Canvas(self.canvasFrame)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=0)
+
+        self.scrollBar = Scrollbar(self.canvasFrame,
+            orient=VERTICAL)
+        self.scrollBar.pack(side=RIGHT, fill=Y, expand=0)
+
+        self.scrollBar.config(command=self.canvas.yview)
+        self.canvas.config(yscrollcommand=self.scrollBar.set)
+
+        self.testButton = Button(self.canvasFrame, text="Test")
+        self.testButtonWindow = self.canvas.create_window(10, 10,
+            anchor=NW, window=self.testButton)
+
+        self.test = NoteSummary(self, 10, 40)
+
+        self.window.lift()
     
-    def getNoteList(self):
+    def getNoteSummaries(self):
         notesDir = self.master.notesDir
         notes = listdir(notesDir)
-        print(notes)
+
+        noteSummaryList = list()
+
+        for note in notes:
+            noteLocation = notesDir + self.master.slashChar + str(note)
+            noteSummary = list()
+
+            with open(noteLocation, "r") as noteContents:
+                for line in noteContents:
+                    tabs = 0
+                    for char in line:
+                        if char == "\t":
+                            tabs += 1
+                    
+                    if tabs == 0:
+                        noteSummary.append(line)
+                    elif tabs == 1:
+                        noteSummary.append(line)
+            
+            noteSummaryList.append([str(note), noteSummary])
+
+        return noteSummaryList
 
     def close(self):
-        self.test.destroy()
+        self.window.destroy()
+
+class NoteSummary():
+    def __init__(self, master, x, y):
+        self.master = master
+
+        self.testButton = Button(self.master.canvasFrame, text="Test")
+        self.testButtonWindow = self.master.canvas.create_window(x, y,
+            anchor=NW, window=self.testButton)
